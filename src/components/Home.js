fe155896c8e6507e3b191ea86b955f73fe76190b
Anastasija <anastasija.miladinovic@gmail.com>
2024-02-21 22:00:54 +0100

    Home Baner-image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,16 @@
 import React from "react";
+import image from "../sky-night.png";
 
 export default function Home () {
-    return <h1>Hpme Page!</h1>
+    return (
+        <main>
+            <img src={image} 
+            alt="Sky" 
+            className="absolute object-cover w-full h-full" 
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-white font-bold cursive leading-none lg:leading-snug home-name">Aloha, I'm Anastasija</h1>
+            </section>
+        </main>
+    );
 }
