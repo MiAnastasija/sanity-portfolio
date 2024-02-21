2644a1e24130005ea0ac58e119d45eeb2dafe400
Anastasija <anastasija.miladinovic@gmail.com>
2024-02-21 15:27:13 +0100

    React Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Post from './components/Post';
+import SinglePost from './components/SinglePost';
+import Projects from './components/Projects';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/post" component={Post} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/post/:slug" component={SinglePost} />
+
+      </Switch>
+    </Router>
   );
 }
 
